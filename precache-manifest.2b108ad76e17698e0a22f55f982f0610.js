0314000acb2ac81cb57981595538ea7b7668f16d
Lucille Blumire <llblumire@gmail.com>
2020-03-21 16:00:48 +0000

    deploy

---
+++ precache-manifest.2b108ad76e17698e0a22f55f982f0610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bdaaafc39e21f23494",
+    "url": "/ose-class-picker/css/chunk-vendors.97d00363.css"
+  },
+  {
+    "revision": "14f6aac452dade597008159df5a03bbf",
+    "url": "/ose-class-picker/index.html"
+  },
+  {
+    "revision": "ca6e25070797751fbefd",
+    "url": "/ose-class-picker/js/app.741b7e99.js"
+  },
+  {
+    "revision": "d0bdaaafc39e21f23494",
+    "url": "/ose-class-picker/js/chunk-vendors.e40b27f2.js"
+  },
+  {
+    "revision": "6f4332ab0b84ec2fd4ece2def2706739",
+    "url": "/ose-class-picker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ose-class-picker/robots.txt"
+  }
+]);
